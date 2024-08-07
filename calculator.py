66bce13bb83ab6de730cb7fb21a2033d61d83e0a
Sangmin Kim <achon100@gmail.com>
2024-08-07 09:53:09 +0900

    Add function of divide

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 
 # 원영덕 2024-08-07 더하기 기능 추가
+# 김상민 2024-08-07 나누기 기능 추가
 def plus(a, b):
     return a + b
 
@@ -8,3 +9,6 @@ def minus(a, b):
 
 def kobsem(a, b):
     return a * b
+
+def divide(a, b):
+    return a / b
